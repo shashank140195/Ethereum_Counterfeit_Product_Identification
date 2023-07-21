9f78b9e682eed1c38807a7c4d0433f81a432bba5
Shashank Gupta <69673535+shashank140195@users.noreply.github.com>
2023-07-20 20:10:54 -0400

    Add files via upload

---
+++ Code/contracts/product.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.8.12;
+
+contract product {
+
+    bytes32[] products;
+    bytes32[] owners;
+    bytes32[] pStatus;
+    bytes32[] Destination;
+
+    mapping(bytes32 => bool) public vProducts;
+
+    function setProduct(bytes32 productId,bytes32 pOwner) public{
+
+        require(!vProducts[productId]);
+        vProducts[productId] = true;
+
+        products.push(productId);
+        owners.push(pOwner);
+        pStatus.push("Available");
+                
+    }
+
+    function viewProducts () public view returns(bytes32[] memory, bytes32[] memory,bytes32[] memory, bytes32[] memory) {
+        return(products,owners,pStatus,Destination);
+    }
+
+    function sellProduct (bytes32 sProductId, bytes32 sDestination) public {
+        bytes32 status;
+        uint i;
+        uint j=0;
+
+        if(products.length>0) {
+            for(i=0;i<products.length;i++) {
+                if(products[i]==sProductId) {
+                    j=i;
+                }
+            }
+        }
+
+        status=pStatus[j];
+        if(status=="Available" && sDestination == "Customer") {
+            pStatus[j]="NA";
+        }
+    }
+
+    function verifyFakeness(bytes32 vProductId) public view returns(bytes32,bytes32,bytes32) {
+
+        bool status=false;
+        uint i;
+        uint j=0;
+
+        if(products.length>0) {
+            for(i=0;i<products.length;i++) {
+                if(products[i]==vProductId) {
+                    j=i;
+                    status=true;
+                }
+            }
+        }
+
+        if(status==true) {
+                if(pStatus[j]=="Available")
+                    return(products[j],owners[j],"Original");
+                else 
+                    return (products[j],owners[j],"Fake");
+        } else {
+                return("NA","NA","Fake");
+        }
+
+    }
+}
